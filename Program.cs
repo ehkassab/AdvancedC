0ea72e64fcb1e5bc0cc1c375166d6263bb6f758a
ehkassab <eh.kassab@gmail.com>
2021-01-17 01:01:58 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdvancedC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
